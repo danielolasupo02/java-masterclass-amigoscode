0f276c02c4906a27434a9d5fc00b14ebc823faea
Daniel Olasupo <danielolasupo02@gmail.com>
2023-06-24 15:54:22 +0100

    Complete lesson 1-20 from java amigoscode

---
+++ java/src/com/daniel/Main.java
@@ -0,0 +1,13 @@
+package com.daniel;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello Daniel 😀 🤣!!!");
+        System.out.println(10 + 10);
+    }
+}
+
+//LESSON 1-20
+//Difference between Static and Dynamic type checking
+//Compiling and Running via Terminal
+//Viewing ByteCode
