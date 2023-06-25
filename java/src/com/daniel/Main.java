86142c7d152499dc4557e8699ebe7fab41a80986
Daniel Olasupo <danielolasupo02@gmail.com>
2023-06-25 22:23:14 +0100

    Complete 1-15 Basic Part-I on W3Resource

--- java/src/com/daniel/Main.java
+++ java/src/com/daniel/Main.java
@@ -1,130 +1,263 @@
 package com.daniel;
-import java.awt.Point;
+import java.util.Scanner;
 
 
 public class Main {
     public static void main(String[] args) {
-//        //primitives
+//        1. Write a Java program to print 'Hello' on screen and your name on a separate line.
+//                Expected Output :
+//        Hello
+//        Alexandra Abramov
+//        System.out.println("Hello \nAlexandra Abramov");
+
+//        2. Write a Java program to print the sum of two numbers.
+//        Test Data:
+//        74 + 36
+//        Expected Output :
+//        110
+//        System.out.println(74+36);
+
+//        3. Write a Java program to divide two numbers and print them on the screen.
+//        Test Data :
+//        50/3
+//        Expected Output :
+//        16
+//        System.out.println(50/3);
+
+//        4. Write a Java program to print the results of the following operations.
+//        Test Data:
+//        a. -5 + 8 * 6
+//        b. (55+9) % 9
+//        c. 20 + -3*5 / 8
+//        d. 5 + 15 / 3 * 2 - 8 % 3
+//        Expected Output :
+//        43
+//        1
+//        19
+//        13
+//        System.out.println(-5 + 8 * 6);
+//        System.out.println((55+9) % 9);
+//        System.out.println(20 + -3*5 / 8);
+//        System.out.println(5 + 15 / 3 * 2 - 8 % 3);
+
+//        5. Write a Java program that takes two numbers as input and displays the product of two numbers.
+//                Test Data:
+//        Input first number: 25
+//        Input second number: 5
+//        Expected Output :
+//        25 x 5 = 125
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Input first number: ");
+//        int fnum = scanner.nextInt();
 //
-//        //integers
-//        int num1 = 20;
-//        int num2 = 50;
-//        int result = num1 + num2;
-//        System.out.println(result);
 //
-//        //double
-//        double pi = 3.14;
-//        System.out.println(pi);
+//        System.out.println("Input last number: ");
+//        int lnum = scanner.nextInt();
 //
-//        //boolean
-//        boolean isAdult = true;
-//        System.out.println(isAdult);
+//        System.out.println("Input first number: " + fnum);
+//        System.out.println("Input second number: " + lnum);
+//        System.out.println(fnum + " X " + lnum + " = " + fnum*lnum);
 //
-//        //character
-//        char thirdLetter = 'c';
-//        System.out.println(thirdLetter);
+//        scanner.close();
+
+//        6. Write a Java program to print the sum (addition), multiply, subtract, divide and remainder of two numbers.
+//                Test Data:
+//        Input first number: 125
+//        Input second number: 24
+//        Expected Output :
+//        125 + 24 = 149
+//        125 - 24 = 101
+//        125 x 24 = 3000
+//        125 / 24 = 5
+//        125 mod 24 = 5
+//        Scanner scanner = new Scanner(System.in);
 //
-//        //byte
-//        byte b = 127;
-//        System.out.println(b);
-
-//        //NUMBER READABILITY
-//        //using underscores for readability of large numbers
-//        int billion = 1_000_000_008;//9 zeros implies integer data type
-//        System.out.println(billion);
-
-//        //ARITHMETIC OPERATIONS
-//        int num1_ = 9;
-//        int num2_ = 18;
-//        System.out.println(num1_ + num2_);
-//        System.out.println(num1_ - num2_);
-//        System.out.println(num1_ * num2_);
-//        System.out.println(num2_ / num1_);
-//        System.out.println(num2_ % num1_);
-
-//        //PRE INCREMENT OPERATOR
-//        int g = 10;
-//        int h = 20;
-//        System.out.println(--g);//result is 9
-//        System.out.println(++h);//result is 21
-
-//        //POST INCREMENT OPERATOR
-//        int g = 10;
-//        int h = 20;
-//        System.out.println(g--);//result is 10
-//        System.out.println(h++);//result is 20
-
-
-//        //BLANK WORD, EMPTY WORD, AND SUBSTRING
-//         String fname = "Daniel";
-//         String lname = "Olasupo";
-//         String blankWord = " ";
-//         String emptyWord = "";
-//        System.out.println(blankWord.isBlank());//returns true because it contains a blank space
-//        System.out.println(emptyWord.isEmpty());//returns true because it contains no character
-//        System.out.println((fname + " " + lname).substring(0, 14));//outputs the 14 characters in the sequence
-//        System.out.println("   a   ".trim());//trims all whitespaces in the character sequence
-
-
-//        //Pass by value
-//        //Example 1
-//        int age = 21;
-//        int ageCopy = age;
-//        System.out.println(age);//result in the stack is 21
-//        System.out.println(ageCopy);//value of age is passed to ageCopy but changing the value of age won't affect ageCopy
-
-          //Example 2
-//          int age = 21;
-//          int ageCopy = increment(age);
-//          System.out.println(ageCopy);
-
-//          //Pass by value with objects
-//          System.out.println("Daniel Olasupo");
+//        System.out.println("Enter first number:");
+//        int fnum = scanner.nextInt();
 //
-//          Point pointA = new Point(10, 20);
-//          Point pointB = pointA;
-//          //Point A & B still point to same object
-//          pointA.x = 100;
-//          pointB.y = 90;
+//        System.out.println("Enter second number:");
+//        int snum = scanner.nextInt();
 //
-//          System.out.println(pointA);//java.awt.Point[x=100,y=90]
-//          System.out.println(pointB);//java.awt.Point[x=100,y=90]
+//        System.out.println(fnum+snum);
+//        System.out.println(fnum-snum);
+//        System.out.println(fnum*snum);
+//        System.out.println(fnum/snum);
+//        System.out.println(fnum%snum);
 //
-//          //Point A & B point to different objects now because Point B creates a brand new object
-//          pointB = new Point(80, 190);
-//          System.out.println(pointA);//java.awt.Point[x=100,y=90]
-//          System.out.println(pointB);//java.awt.Point[x=80,y=190]
+//        scanner.close();
 
-            //Arrays
-            //looping through an array of numbers
-//            int[] num = new int[3];
-//            num[1] = 1;
-//            num[2] = 2;
-//            for (int i :  num) {
-//                System.out.println(i);
-//            }
+//        7. Write a Java program that takes a number as input and prints its multiplication table up to 10.
+//        Test Data:
+//        Input a number: 8
+//        Expected Output :
+//        8 x 1 = 8
+//        8 x 2 = 16
+//        8 x 3 = 24
+//...
+//        8 x 10 = 80
 
-//            //looping through an array of numbers
-//            int[] numbers = {1, 2, 3, 4, 5, 6};
-//            for (int i : numbers) {
-//                System.out.println(i);
-//            }
+//        Scanner scanner = new Scanner(System.in);
+//
+//        System.out.println("Enter the number you'd like to see all its multiple: ");
+//        int number = scanner.nextInt();
+//
+//        for (int i = 1; i <=10; i++)
+//            System.out.println(number + " x " + i + " = " + number * i );
 
-//              //looping through an array of strings
-//              String[] siblings = {"Toyin","David", "Praise", "Daniel", "Judah"};
-//              for (String i : siblings) {
-//                  System.out.println(i);
-//              }
+//        8. Write a Java program to display the following pattern.
+//                Sample Pattern :
+//
+//        J    a   v     v  a
+//        J   a a   v   v  a a
+//        J  J  aaaaa   V V  aaaaa
+//        JJ  a     a   V  a     a
+//        System.out.println("   J    a   v     v  a                                                  ");
+//        System.out.println("   J   a a   v   v  a a   ");
+//        System.out.println("J  J  aaaaa   V V  aaaaa ");
+//        System.out.println(" JJ  a     a   V  a     a");
+
+//        9. Write a Java program to compute the specified expressions and print the output.
+//                Test Data:
+//        ((25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5))
+//        Expected Output
+//        2.138888888888889
+// Scanner scanner = new Scanner(System.in);
+//System.out.println("Enter the first number");
+// double a = scanner.nextDouble();
+//System.out.println("Enter the second number");
+// double b = scanner.nextDouble();
+//System.out.println("Enter the third number");
+// double c = scanner.nextDouble();
+//System.out.println("Enter the fourth number");
+// double d = scanner.nextDouble();
+// System.out.println((a * b - b * b) / (c - d));
+
+//        10. Write a Java program to compute a specified formula.
+//                Specified Formula :
+//        4.0 * (1 - (1.0/3) + (1.0/5) - (1.0/7) + (1.0/9) - (1.0/11))
+//        Expected Output
+//        2.9760461760461765
+
+//        System.out.println(4.0 * (1 - (1.0/3) + (1.0/5) - (1.0/7) + (1.0/9) - (1.0/11)));
+
+//        11. Write a Java program to print the area and perimeter of a circle.
+//                Test Data:
+//        Radius = 7.5
+//        Expected Output
+//        Perimeter is = 47.12388980384689
+//        Area is = 176.71458676442586
+
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Enter radius of circle: ");
+//        double radius = scanner.nextDouble();
+//        System.out.println("Perimeter of circle: " + 2*Math.PI*radius);
+//        System.out.println("Radius of circle: " + Math.PI*radius*radius);
+
+//       12. Write a Java program that takes three numbers as input to
+//       calculate and print the average of the numbers.
+//
+//
+//                Scanner scanner = new Scanner(System.in);
+//
+//                //collect all three inputs
+//                System.out.println("Enter first number: ");
+//                double fnum = scanner.nextDouble();
+//                System.out.println("Enter second number: ");
+//                double snum = scanner.nextDouble();
+//                System.out.println("Enter third number: ");
+//                double tnum = scanner.nextDouble();
+//
+//                //store the inputs in an array
+//                double[] numbers = {fnum, snum, tnum};
+//
+//                //initialize the sum of the array to zero
+//                double sum = 0;
+//
+//                //compute the length of the array
+//                int size = numbers.length;
+//
+//                //loop through all the arrays and find their sum
+//                for (double i = 0; i <= numbers.length; i++)
+//                    sum += i;
+//                System.out.println(sum);
+//
+//                //compute the average of the three numbers
+//                double average = sum/size;
+//                System.out.println(average);
+
+//        13. Write a Java program to print the area and perimeter of a rectangle.
+//                Test Data:
+//        Width = 5.5 Height = 8.5
+//        Expected Output
+//        Area is 5.6 * 8.5 = 47.60
+//        Perimeter is 2 * (5.6 + 8.5) = 28.20
+//        Scanner scanner = new Scanner(System.in);
+//
+//        System.out.println("Enter the width of the rectangle: ");
+//        float width = scanner.nextFloat();
+//
+//        System.out.println("Enter the height of the rectangle: ");
+//        float height = scanner.nextFloat();
+//
+//        System.out.println("Area of rectangle: " + width*height);
+//        System.out.println("Perimeter of rectangle: " + 2*(width+height));
+
+//        14. Write a Java program to print an American flag on the screen.
+//        Expected Output
+//
+//* * * * * * ==================================
+// * * * * *  ==================================
+//* * * * * * ==================================
+// * * * * *  ==================================
+//* * * * * * ==================================
+// * * * * *  ==================================
+//* * * * * * ==================================
+// * * * * *  ==================================
+//* * * * * * ==================================
+//==============================================
+//==============================================
+//==============================================
+//==============================================
+//==============================================
+//==============================================
+//        System.out.println("* * * * * * ==================================");
+//        System.out.println(" * * * * *  ==================================");
+//        System.out.println("* * * * *  ==================================");
+//        System.out.println(" * * * * *  ==================================");
+//        System.out.println("* * * * * * ==================================");
+//        System.out.println(" * * * * *  ==================================");
+//        System.out.println("* * * * * * ==================================");
+//        System.out.println(" * * * * *  ==================================");
+//        System.out.println("* * * * * * ==================================");
+//        System.out.println("==============================================");
+//        System.out.println("==============================================");
+//        System.out.println("==============================================");
+//        System.out.println("==============================================");
+//        System.out.println("==============================================");
+//        System.out.println("==============================================");
+
+//        15. Write a Java program to swap two variables.
+//int a = 9;
+//int b = 10;
+//int c;
+//c = a; //c = 9
+//a = b; //a = 10
+//b = c; //b = 9
+//        System.out.println("New Value of a: " + a);
+//        System.out.println("New Value of b: " + b);
+
+
+
+
+
+
+
 
 
 
 
     }
-    //meant to be placed outside the main method
-    //function to increase age by 1
-//    static int increment(int age) {
-//        return ++age;
-//    }
+
 }
 
 //LESSON 21-40
