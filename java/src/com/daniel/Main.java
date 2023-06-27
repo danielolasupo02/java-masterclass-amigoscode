4290e5b225fc4c818d86c296889648512e4eafbd
Daniel Olasupo <danielolasupo02@gmail.com>
2023-06-27 22:43:28 +0100

    Incomplete 34 Basic Part-I on W3Resource

--- java/src/com/daniel/Main.java
+++ java/src/com/daniel/Main.java
@@ -40,42 +40,147 @@ public class Main {
 //                long sum = addBinary(binary1, binary2);
 //
 //                System.out.println("Sum of the binary numbers: " + sum);
-         }
 
-//            public static long addBinary(long binary1, long binary2) {
-//                long sum = 0;
-//                int carry = 0;
-//                int digitPosition = 1;
+
+//        31. Write a Java program to check whether Java is installed on your computer.
+//                try {
+//                    // Execute the command "java -version" in the command prompt
+//                    Process process = Runtime.getRuntime().exec("java -version");
 //
-//                while (binary1 != 0 || binary2 != 0) {
-//                    int bit1 = (int)(binary1 % 10);
-//                    int bit2 = (int)(binary2 % 10);
+//                    // Wait for the process to finish
+//                    int exitCode = process.waitFor();
 //
-//                    int digitSum = bit1 + bit2 + carry;
-//                    int bitResult = digitSum % 2;
-//                    carry = digitSum / 2;
-//
-//                    sum += bitResult * digitPosition;
-//
-//                    digitPosition *= 10;
-//
-//                    binary1 /= 10;
-//                    binary2 /= 10;
+//                    // Check the exit code to determine if Java is installed
+//                    if (exitCode == 0) {
+//                        System.out.println("Java is installed.");
+//                    } else {
+//                        System.out.println("Java is not installed.");
+//                    }
+//                } catch (Exception e) {
+//                    // Handle any exceptions that may occur
+//                    System.out.println("Java is not installed or an error occurred.");
+//                    e.printStackTrace();
 //                }
+//            }
+
+//        32. Write a Java program to compare two numbers.
+//        Input Data:
+//        Input first integer: 25
+//        Input second integer: 39
+//        Expected Output
+//        25 != 39
+//        25 < 39
+//        25 <= 39
+//        Scanner scanner = new Scanner(System.in);
 //
-//                if (carry != 0) {
-//                    sum += carry * digitPosition;
-//                }
+//        System.out.println("Enter first number");
+//        int num1 = scanner.nextInt();
 //
-//                return sum;
+//        System.out.println("Enter second number");
+//        int num2 = scanner.nextInt();
 //
+//        if (num1 != num2)
+//            System.out.println(num1 + " is not equal to " + num2);
+//        if (num1 < num2)
+//            System.out.println(num1 + " is less than " + num2);
+//        if (num1 <= num2) {
+//            System.out.println(num1 + " is less than or equal to " + num2);
+//        }
+//       else
+//            System.out.println("Out of comparison scope");
+
+//        33. Write a Java program and compute the sum of an integer's digits.
+//        Input Data:
+//        Input an integer: 25
+//        Expected Output
 //
+//        The sum of the digits is: 7
+//        100
 //
+//        --first cycle--
+//                number = 100;
 //
+//        int digit = 100/10 = 10 R 0;
+//        digit = 0;
 //
+//        sum = 0;
+//
+//        number = 100/10 = 10;
+//
+//        --second cycle--
+//        number = 10;
+//
+//        int digit = 10/10 = 1 R 0;
+//        digit = 0;
+//
+//        sum = 0;
+//
+//        number = 1;
+//
+//        --last cycle--
+//        number = 1;
+//
+//        digit = 1%10 = 1;
+//
+//        sum = 1;
+//
+//        number = 1/10 = 0;
+//
+//        break out of looP
+//        SUM is 1
+
+//
+//        Scanner scanner = new Scanner(System.in);
+//
+//        System.out.print("Input an integer: ");
+//        int number = scanner.nextInt();
+//
+//        int sum = computeDigitSum(number);
+//
+//        System.out.println("Sum of the digits: " + sum);
+
+//        34. Write a Java program to compute hexagon area.
+//        Area of a hexagon = (6 * s^2)/(4*tan(π/6))
+//        where s is the length of a side
+//        Input Data:
+//        Input the length of a side of the hexagon: 6
+//        Expected Output
+//
+//        The area of the hexagon is: 93.53074360871938
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the length of the hexagon: ");
+        int hexLength = scanner.nextInt();
+
+        double hexArea = ((6 * (hexLength*hexLength)) / (4*(Math.tan(Math.toDegrees(Math.PI/6)))));
+
+        System.out.println("The area of the hexagon is : " + hexArea);
+
+
+
+
+
+
+
+
+
+    }
+
+//    public static int computeDigitSum(int number) {
+//        int sum = 0;
+//
+//        while (number != 0) {
+//            int digit = number % 10;
+//            sum += digit;
+//            number /= 10;
+//        }
+//
+//        return sum;
 //    }
 
 }
 
-//LESSON 16-30
+
+//LESSON 31-40
 //CHALLENGING ==> 17 - 30 (Very Hard)
