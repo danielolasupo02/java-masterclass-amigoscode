21d32811d3a41de3a176e43e3012bb13dfd6d692
Daniel Olasupo <danielolasupo02@gmail.com>
2023-07-04 10:26:56 +0100

    Complete Implicit and Explicit Type Casting Lesson from java-amigoscode

--- java/src/com/daniel/Main.java
+++ java/src/com/daniel/Main.java
@@ -7,49 +7,26 @@ public class Main {
 
     public static void main(String[] args) {
 
+       //IMPLICIT TYPE CASTING -- WIDENING
+        System.out.println("IMPLICIT TYPE CASTING");
+        int balance = 1_000;
+        double balanceToDouble = balance;
+        System.out.println(balance);
+        System.out.println(balanceToDouble);
 
-//        calcIncomeSummary(1300, 250000);
-//
-//        int[] nums = {1, 2, 3, 4, 5, 6, 7, 8};
-//
-//
-//
-//
-//
-//        for (int i = 0; i < nums.length; i++){
-//            if (nums[i] == 5) {
-//                return;
-//            }
-//            System.out.println(nums[i]);
-//
-//        }
+        //EXPLICIT TYPE CASTING -- NARROWING
+        System.out.println("EXPLICIT TYPE CASTING");
+        double doubleBalance = 10000.0005;
+        int doubleBalanceToInt = (int) doubleBalance;
+        System.out.println(doubleBalance);
+        System.out.println(doubleBalanceToInt);
 
-        final var name = "Daniel";
-//        name ="Praise";
-        System.out.println(name);
 
 
     }
 
-//    public static void calcIncomeSummary(double moIncome, double price) {
-//        double monthlyIncome = moIncome;
-//        double annualIncome = monthlyIncome * 12;
-//        double purchasePrice = price;
-//        double annualYield = (annualIncome/purchasePrice)*100;
-//        System.out.println("Your monthly rental income is: £" + monthlyIncome);
-//        System.out.println("Your annual rental income is: £" + annualIncome);
-//        System.out.println("You purchased the property for: £" + purchasePrice);
-//        System.out.println("Your rental yield is: £" + Math.round(annualYield) + "%");
-//
-//
-//    }
+
 }
 
-//56 - equalsignore => ignores case
-//60 - SWITCH case DEFAULT with break statement
-//61 - SWITCH case with return
-//63 - PACKAGES --> CHECKING the file path for SOURCE folder
-//64 - Creating a PACKAGE - It works like creating a folder to contain files
-//   - but in this case we are working with Java files
-//67 - CTRL + ALT + O ==> Shortcut to optimize imports
+
 
