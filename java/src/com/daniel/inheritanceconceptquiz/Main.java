e99bfd03cf1229c443ba2d783e6edd5647d738e5
Daniel Olasupo <danielolasupo02@gmail.com>
2023-07-18 10:55:00 +0100

    Create package for inheritance section in Java W3Resources

---
+++ java/src/com/daniel/inheritanceconceptquiz/Main.java
@@ -0,0 +1,9 @@
+package com.daniel.inheritanceconceptquiz;
+
+public class Main {
+    public static void main(String[] args) {
+//        1. Write a Java program to create a class called Person
+//        with private instance variables name, age. and country. Provide
+//        public getter and setter methods to access and modify these variables.
+    }
+}
